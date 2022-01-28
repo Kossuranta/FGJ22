8ae51c67a72d5772233bb63d0b9136807bb04c1d
Kossuranta <kossuranta@gmail.com>
2022-01-28 23:40:00 +0200

    Coloring animation with mask

--- Assets/Scripts/ColorComponent.cs
+++ Assets/Scripts/ColorComponent.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class ColorComponent : MonoBehaviour
@@ -5,7 +6,18 @@ public class ColorComponent : MonoBehaviour
     [SerializeField]
     ColorEnum colorSetting = ColorEnum.None;
 
+    [SerializeField]
+    Transform colorMask = null;
+
     bool isActive = false;
+    float colorMaskFill = 0;
+    float colorMaskStartPosY = 0;
+    
+    void Awake()
+    {
+        enabled = false;
+        colorMaskStartPosY = colorMask.localPosition.y;
+    }
 
     public void Activate(ColorEnum color)
     {
@@ -13,10 +25,21 @@ public class ColorComponent : MonoBehaviour
         if (!colorSetting.HasFlag(color)) return;
 
         isActive = true;
+        enabled = true;
+    }
 
-        // Do stuff to show colored version
-        
-        return;
+    
+    void Update()
+    {
+        colorMaskFill += Time.deltaTime / 5f;
+        Vector3 colorMaskPos = colorMask.localPosition;
+        colorMaskPos.y = Mathf.Lerp(colorMaskStartPosY, 0f, colorMaskFill);
+        colorMask.localPosition = colorMaskPos;
+
+        if (colorMaskFill >= 1)
+        {
+            enabled = false;
+        }
     }
 
     public ColorEnum ColorSetting { get { return colorSetting; } }
