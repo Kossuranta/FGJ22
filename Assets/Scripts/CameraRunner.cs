58b7a017f287c20b1113547e0eb5013ba02fdb75
Kossuranta <kossuranta@gmail.com>
2022-01-29 02:19:04 +0200

    Set CameraRunner to get player on Awake.

--- Assets/Scripts/CameraRunner.cs
+++ Assets/Scripts/CameraRunner.cs
@@ -1,13 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+using TarodevController;
 using UnityEngine;
 
 public class CameraRunner : MonoBehaviour
 {
-    public Transform player;
-    void Update () 
-    {
-        transform.position = new Vector3 (player.position.x + 6, player.position.y + 3, -10);
+    Transform player;
 
+    void Awake()
+    {
+        player = FindObjectOfType<PlayerController>().transform;
+    }
+
+    void Update ()
+    {
+        Vector2 playerPos = player.position;
+        transform.position = new Vector3 (playerPos.x + 6, playerPos.y + 3, -10);
     }
 }
