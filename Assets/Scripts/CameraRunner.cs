2d960f8862037126e67308de0c9f7b4ada47fe8b
Kossuranta <kossuranta@gmail.com>
2022-01-29 12:23:18 +0200

    Revert CameraRunner changes

--- Assets/Scripts/CameraRunner.cs
+++ Assets/Scripts/CameraRunner.cs
@@ -7,7 +7,6 @@ public class CameraRunner : MonoBehaviour
 
     void Awake()
     {
-<<<<<<< Updated upstream
         player = FindObjectOfType<PlayerController>().transform;
     }
 
@@ -15,8 +14,5 @@ public class CameraRunner : MonoBehaviour
     {
         Vector2 playerPos = player.position;
         transform.position = new Vector3 (playerPos.x + 6, playerPos.y + 3, -10);
-=======
-        transform.position = new Vector3 (player.position.x + 6, player.position.y + 3, -10);
->>>>>>> Stashed changes
     }
 }
