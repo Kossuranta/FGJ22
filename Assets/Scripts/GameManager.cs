d43f3f69dc2fca486db2afa8e5d27bda7d980073
Kossuranta <kossuranta@gmail.com>
2022-01-29 19:54:19 +0200

    Add ColorManager stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,18 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = null;
-    
-    ColorSystem colorSystem = null;
+
+    [SerializeField]
+    ColorEnum enabledColors = ColorEnum.Red;
+
+    [SerializeField]
+    ColorEnum debugEnableColor = ColorEnum.None;
+
+    ColorManager colorManager = null;
 
     void Awake()
     {
@@ -15,16 +22,20 @@ public class GameManager : MonoBehaviour
             return;
         }
         
-        DontDestroyOnLoad(this);
         Instance = this;
-        Setup();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        
+        DontDestroyOnLoad(this);
     }
 
-    void Setup()
+    void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
     {
-        colorSystem = new ColorSystem();
-        colorSystem.Setup();
+        colorManager = FindObjectOfType<ColorManager>();
+        if (colorManager != null)
+            colorManager.Setup(this);
     }
+    
     public void EnterLevel(Levels level)
     {
         LastLevel = CurrentLevel;
@@ -39,10 +50,62 @@ public class GameManager : MonoBehaviour
             case Levels.Level_01:
                 SceneManager.LoadScene("Level_01");
                 break;
+            
+            case Levels.Level_02:
+                SceneManager.LoadScene("Level_02");
+                break;
+            
+            case Levels.Level_03:
+                SceneManager.LoadScene("Level_03");
+                break;
+        }
+    }
+
+    public void OnBlenderCompleted()
+    {
+        ColorEnum colorToBeEnabled = ColorEnum.None;
+        switch (LastLevel)
+        {
+            case Levels.Level_01:
+                colorToBeEnabled = ColorEnum.Green;
+                break;
+            case Levels.Level_02:
+                colorToBeEnabled = ColorEnum.Purple;
+                break;
+            case Levels.Level_03:
+                colorToBeEnabled = ColorEnum.Orange;
+                break;
+        }
+
+        EnableColor(colorToBeEnabled);
+    }
+
+    public void EnableColor(ColorEnum color)
+    {
+        if (colorManager == null)
+        {
+            Debug.LogError($"Trying to enabled color {color}, but colorManager is null!");
+            return;
+        }
+        
+        if (enabledColors.HasFlag(color))
+        {
+            enabledColors |= color;
+            colorManager.EnableColor(color);
+        }
+    }
+
+    void Update()
+    {
+        if (debugEnableColor != ColorEnum.None)
+        {
+            EnableColor(debugEnableColor);
+            debugEnableColor = ColorEnum.None;
         }
     }
 
     public Levels LastLevel { get; private set; } = Levels.Hub;
     public Levels CurrentLevel { get; private set; } = Levels.Hub;
+    public ColorEnum EnabledColors { get { return enabledColors; } }
 }
 
